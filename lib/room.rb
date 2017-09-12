439049737464ab4b924cb058fb9a0c0a1a67a557
Daniel Nollette and Kristen Kulha <daniel@>
2017-09-12 14:54:46 -0700

    add further functionality to Room class so it passes all current backend tests.

--- lib/room.rb
+++ lib/room.rb
@@ -1,6 +1,25 @@
 class Room
+  @@rooms = []
   attr_reader(:left, :right, :up, :down, :row, :column)
   attr_accessor(:here_before)
+
+  def self.all()
+    @@rooms
+  end
+
+  def self.clear()
+    @@rooms = []
+  end
+
+  def self.find(row, column)
+    room_row = row
+    room_col = column
+    @@rooms.each do |room|
+      if room.row == room_row && room.column == room_col
+        return room
+      end
+    end
+  end
 
   def initialize(left, right, up, down, row, column)
     @left = left
@@ -10,6 +29,6 @@ class Room
     @row = row
     @column = column
     @here_before = false
+    @@rooms.push(self)
   end
-
 end
--- spec/room_spec.rb
+++ spec/room_spec.rb
@@ -2,6 +2,10 @@ require('room')
 require('rspec')
 
 describe('room')do
+  before() do
+    Room.clear()
+  end
+
   it("returns false if the room doesn't have the option of leaving to the left") do
     test_room = Room.new(false, true, true, true, 0, 0)
     expect(test_room.left).to(eq(false))
@@ -18,47 +22,53 @@ describe('room')do
     test_room = Room.new(true, true, true, false, 0, 0)
     expect(test_room.down).to(eq(false))
   end
-  # it("returns false if the room hasn't been visited ") do
-  #   test_room = Room.new(false, false, false, false, 0, 0)
-  #   expect(test_room.visited?).to(eq(false))
-  # end
-  # it("returns true if the room has been visited ") do
-  #   test_room = Room.new(false, false, false, false, 0, 0)
-  #   test_room.visited
-  #   expect(test_room.visited?).to(eq(true))
-  # end
-  # it("returns false if the room doesn't have the option of leaving to the left") do
-  #   test_room = Room.new(true, true, true, true, 0, 0)
-  #   expect(test_room.left?).to(eq(true))
-  # end
-  # it("returns true if the room doesn't have the option of leaving to the right") do
-  #   test_room = Room.new(true, true, true, true, 0, 0)
-  #   expect(test_room.right?).to(eq(true))
-  # end
-  # it("returns true if the room doesn't have the option of leaving to the up") do
-  #   test_room = Room.new(true, true, true, true, 0, 0)
-  #   expect(test_room.up?).to(eq(true))
-  # end
-  # it("returns true if the room doesn't have the option of leaving to the down") do
-  #   test_room = Room.new(true, true, true, true, 0, 0)
-  #   expect(test_room.down?).to(eq(true))
-  # end
-  # it("returns row number of the given room") do
-  #   test_room = Room.new(true, true, true, true, 3, 4)
-  #   expect(test_room.row).to(eq(3))
-  # end
-  # it("returns column number of the given room") do
-  #   test_room = Room.new(true, true, true, true, 3, 4)
-  #   expect(test_room.col).to(eq(4))
-  # end
+  it("returns false if the room hasn't been visited ") do
+    test_room = Room.new(false, false, false, false, 0, 0)
+    expect(test_room.here_before).to(eq(false))
+  end
+  it("returns true if the room has been visited ") do
+    test_room = Room.new(false, false, false, false, 0, 0)
+    test_room.here_before = true
+    expect(test_room.here_before).to(eq(true))
+  end
+  it("returns ture if the room has the option of leaving to the left") do
+    test_room = Room.new(true, false, false, false, 0, 0)
+    expect(test_room.left).to(eq(true))
+  end
+  it("returns true if the room has the option of leaving to the right") do
+    test_room = Room.new(false, true, false, false, 0, 0)
+    expect(test_room.right).to(eq(true))
+  end
+  it("returns true if the room has the option of leaving to the up") do
+    test_room = Room.new(false, false, true, false, 0, 0)
+    expect(test_room.up).to(eq(true))
+  end
+  it("returns true if the room has the option of leaving to the down") do
+    test_room = Room.new(false, false, false, true, 0, 0)
+    expect(test_room.down).to(eq(true))
+  end
+  it("returns row number of the given room") do
+    test_room = Room.new(true, true, true, true, 3, 4)
+    expect(test_room.row).to(eq(3))
+  end
+  it("returns column number of the given room") do
+    test_room = Room.new(true, true, true, true, 3, 4)
+    expect(test_room.column).to(eq(4))
+  end
 end
-# describe('room.find') do
-#   it("finds a room based on its row and column") do
-#     test_room1 = Room.new(true, true, true, true, 2, 4)
-#     test_room2 = Room.new(true, true, true, true, 3, 1)
-#     expect(Room.find(2, 4)).to(eq(test_room1))
-#     expect(Room.find(3, 1)).to(eq(test_room2))
-#   end
-# end
+
+describe('room.@@rooms') do
+  it("adds room to rooms array when it's created.") do
+    test_room = Room.new(true, true, true, true, 3, 4)
+    expect(Room.all().include?(test_room)).to(eq(true))
+  end
+end
+
+describe('room.find') do
+  it("finds a room based on its row and column") do
+    test_room1 = Room.new(true, true, true, true, 2, 4)
+    expect(Room.find(2, 4)).to(eq(test_room1))
+  end
+end
 
   #left right up down row column here_before
